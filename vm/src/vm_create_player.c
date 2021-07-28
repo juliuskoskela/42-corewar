0750d9eb341f4ae4d8a34ab8c2e8d10349885c16
ksuomala <ksuomala@student.hive.fi>
2021-07-28 16:04:44 +0300

    Norme

---
+++ vm/src/vm_create_player.c
@@ -0,0 +1,70 @@
+#include "vm.h"
+
+static void	vm_read_header(t_arena *arena, t_uint32 player_number, int fd)
+{
+	t_player	*player;
+	t_byte		buf[COMMENT_LENGTH];
+
+	mzero(buf, CHAMP_MAX_SIZE);
+	player = &arena->all_players[player_number];
+	if (read(fd, buf, sizeof(t_byte) * 4) != 4)
+		vm_error("Invalid bytes in input file\n");
+	player->header.magic = *(t_uint32 *)vm_reverse_bytes(\
+		&player->header.magic, (void *)buf, sizeof(t_uint32));
+	if (read(fd, buf, sizeof(t_byte) * PROG_NAME_LENGTH) != PROG_NAME_LENGTH)
+		vm_error("Invalid bytes in input file\n");
+	s_cpy(player->header.prog_name, (const char *)buf);
+	if (read(fd, buf, sizeof(t_byte) * 4) != 4)
+		vm_error("Invalid bytes in input file\n");
+	if (read(fd, buf, sizeof(t_byte) * 4) != 4)
+		vm_error("Invalid bytes in inputfilen\n");
+	player->header.prog_size = *(t_uint32 *)vm_reverse_bytes(\
+		(void *)&player->header.prog_size, (void *)buf, sizeof(t_uint32));
+	if (read(fd, buf, COMMENT_LENGTH) != COMMENT_LENGTH)
+		vm_error("Invalid bytes in inputfilen\n");
+	s_cpy(player->header.comment, (const char *)buf);
+	if (read(fd, buf, sizeof(t_byte) * 4) != 4)
+		vm_error("Invalid bytes in inputfilen\n");
+}
+
+/*
+** Reading the last prog_size bytes of the input file and saving them to 
+** arena->mem. Checking that the size of the file == prog_size.
+** offset (MEM_SIZE / player_count) bytes of memory will be available for each
+** player.
+*/
+
+static void	vm_read_program(t_arena *arena, t_uint32 player_number, int fd)
+{
+	t_uint32	player_mem_location;
+	t_size		check;
+
+	player_mem_location = (player_number - 1) * arena->offset;
+	check = read(fd, arena->mem + player_mem_location, \
+		arena->all_players[player_number].header.prog_size + 1);
+	if (check != arena->all_players[player_number].header.prog_size)
+		vm_error("Invalid amount of bytes in program\n");
+}
+
+void	vm_create_player(t_arena *arena, t_uint32 *player_number, char *name)
+{
+	t_player	player;
+	int			fd;
+
+	if (arena->all_players[*player_number].number)
+		*player_number += 1;
+	if (*player_number > MAX_PLAYERS)
+		vm_error("player_number is not within MAX_PLAYERS\n");
+	mzero(&player, sizeof(t_player));
+	player.number = *player_number;
+	arena->all_players[*player_number - 1] = player;
+	if (s_cmp(".cor", s_rchr(name, '.')))
+		vm_error("Champions must be .cor files\n");
+	fd = open(name, O_RDONLY);
+	if (fd < 0)
+		vm_error("Unable to open .cor file \n");
+	vm_read_header(arena, *player_number, fd);
+	vm_read_program(arena, *player_number, fd);
+	*player_number += 1;
+	close(fd);
+}
---
+++ vm/src/vm_save_input.c
@@ -0,0 +1,86 @@
+#include "vm.h"
+
+/*
+** Skips flags (-n and -dump) and their following argument.
+** other arguments are counted as players.
+*/
+
+static t_uint32	vm_count_players(t_uint32 argc, char **argv)
+{
+	t_uint32	i;
+	t_uint32	player_count;
+
+	i = 0;
+	player_count = 0;
+	while (++i < argc)
+	{
+		if (s_cmp(argv[i], "-n") && s_cmp(argv[i], "-dump"))
+			player_count++;
+		else
+			i++;
+	}
+	return (player_count);
+}
+
+/*
+**	Checking that the arguments given to the vm are valid. Saving the room
+**	numbers to all_players[].number defined by the -n flag. -n and -dump
+**	flags must be followed by a numeric argument.
+*/
+
+static void	vm_validate_input(t_arena *arena, t_uint32 argc, char **argv)
+{
+	t_uint32	i;
+	t_uint32	set_nbr;
+
+	i = 0;
+	while (++i < argc)
+	{
+		if (argv[i][0] == '-' && i + 1 == argc)
+			vm_error("No argument after -flag\n");
+		if (!s_cmp(argv[i], "-n"))
+		{
+			set_nbr = s_toi(argv[++i]);
+			if (set_nbr <= 0 || set_nbr > MAX_PLAYERS)
+				vm_error("Invalid value after -n flag\n");
+			if (arena->all_players[set_nbr - 1].number)
+				vm_error("All champions must be given unique numbers\n");
+			s_cpy(arena->all_players[set_nbr - 1].header.prog_name, argv[++i]);
+			arena->all_players[set_nbr - 1].number = set_nbr;
+		}
+		else if (!s_cmp(argv[i], "-dump"))
+		{
+			arena->dump_nbr_cycles = s_toi(argv[++i]);
+			if (!arena->dump_nbr_cycles)
+				vm_error("Invalid value after -dump\n");
+		}
+	}
+}
+
+/*
+** vm_validate_input checks the arguments -n and -dump. In this loop flags and
+** their arguments are skipped. Players are saved in argument order to the first
+** available spot in arena->all_players. Player numbers can be assigned with the 
+** -n flag.
+*/
+
+void	vm_save_input(t_arena *arena, t_uint32 argc, char **argv)
+{
+	t_uint32	i;
+	t_uint32	player_number;
+
+	i = 1;
+	player_number = 1;
+	vm_validate_input(arena, argc, argv);
+	arena->player_count = vm_count_players(argc, argv);
+	arena->offset = MEM_SIZE / arena->player_count;
+	while (i < argc)
+	{
+		if (!s_cmp(argv[i], "-n"))
+			i += 3;
+		else if (!s_cmp(argv[i], "-dump"))
+			i += 2;
+		else
+			vm_create_player(arena, &player_number, argv[i++]);
+	}
+}
