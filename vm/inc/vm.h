4ce75c875ba73806ca3a69b834bc67e246703f40
ksuomala <ksuomala@student.hive.fi>
2021-07-29 13:55:01 +0300

    divide functions to files

--- vm/inc/vm.h
+++ vm/inc/vm.h
@@ -17,5 +17,6 @@ void	vm_error(const char *message);
 void	vm_save_input(t_arena *arena, t_uint32 argc, char **argv);
 void	vm_create_player(t_arena *arena, t_uint32 *player_number, char *name);
 void	*vm_reverse_bytes(void *dst, void *src, t_size size);
+void	vm_test_print_arena(t_arena arena);
 
 #endif
