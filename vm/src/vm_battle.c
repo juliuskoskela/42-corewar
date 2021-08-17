62e0dd518bb2d24069ee0c91679b2e1590271234
ksuomala <ksuomala@student.hive.fi>
2021-08-17 19:42:04 +0300

    modify create_process

--- vm/src/vm_battle.c
+++ vm/src/vm_battle.c
@@ -14,27 +14,6 @@ t_process	*vm_free_processes(t_process **lst)
 	return (NULL);
 }
 
-/*
-** Adds a t_process element to the end of the list.
-*/
-
-t_process	*vm_create_process(t_arena arena, t_process *process_lst, \
-int player_id)
-{
-	t_process	*new_process;
-
-	new_process = minit(sizeof(t_process));
-	if (!new_process)
-		vm_error("Malloc failed in create_process\n");
-	new_process->id = player_id;
-	new_process->registers[0] = player_id;
-	new_process->pc = (player_id - 1) * arena.offset;
-//	new_process->pc = &arena.mem[MEM_SIZE / arena.player_count * player_id];
-	new_process->cycles_before_execution = -1;
-	new_process->next = process_lst;
-	return (new_process);
-}
-
 t_process	*init_processes(t_arena arena)
 {
 	t_size		player_id;
@@ -43,7 +22,7 @@ t_process	*init_processes(t_arena arena)
 	process_lst = NULL;
 	player_id = 0;
 	while (++player_id <= arena.player_count)
-		process_lst = create_process(arena, process_lst, player_id);
+		process_lst = vm_create_process(arena, process_lst, player_id);
 	return (process_lst);
 }
 
