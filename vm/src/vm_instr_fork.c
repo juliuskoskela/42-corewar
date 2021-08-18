d950eee0fcfb256e15f6b0df76d728844d462d88
ksuomala <ksuomala@student.hive.fi>
2021-08-18 17:34:20 +0300

    fork instruction

--- vm/src/vm_instr_fork.c
+++ vm/src/vm_instr_fork.c
@@ -1,20 +1,22 @@
 #include "vm.h"
 
-// Get the id for the new process from the first T_DIR argument.
+// Takes one argument of type T_DIR.
+//
 // Copy the memory of the current process.
-// Modify the pc of the new process.
+// Set the PC at (PC + (1st parameter % IDX_MOD)).
 
 void	vm_instr_fork(
 		t_arena *a,
 		t_process *p)
 {
 	t_size		mem_i;
-	t_uint64	id;
+	t_uint64	offset;
 
 	mem_i = p->pc;
 	mem_i = (mem_i + 1) % MEM_SIZE;
-	id = vm_get_val(a, p, T_DIR, &mem_i);
+	offset = vm_get_val(a, p, T_DIR, &mem_i);
 	vm_create_process(*a, a->processes, p->id);
 	mcpy(a->processes, p, sizeof(t_process));
-	a->processes->pc = p->pc + (id % IDX_MOD);
+	a->processes->pc = p->pc + (offset % IDX_MOD);
+	p->pc =+ DIR_VAL_SIZE;
 }
