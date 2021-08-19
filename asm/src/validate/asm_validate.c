c35a44ea2615c41c023dc68d2cba3fe91e7c094b
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-08-19 12:30:10 +0300

    asm: print verbose only if error has not occurred

--- asm/src/validate/asm_validate.c
+++ asm/src/validate/asm_validate.c
@@ -7,7 +7,7 @@ int	asm_validate_ast(t_output_data *data, t_astnode *tree, int verbose)
 	int	ret;
 
 	ret = asm_visit_program(tree, &data->symbols, &data->header);
-	if (verbose)
+	if (ret && verbose)
 		asm_print_symbol_list(&data->symbols,
 			"Symbol table after first pass through AST:");
 	return (ret);
