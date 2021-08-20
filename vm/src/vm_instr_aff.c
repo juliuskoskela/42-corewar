947c0d333324a234948759bcb68ece7b687fb805
ksuomala <ksuomala@student.hive.fi>
2021-08-20 14:05:23 +0300

    aff instruction

---
+++ vm/src/vm_instr_aff.c
@@ -0,0 +1,26 @@
+#include "vm.h"
+
+// Converts the value of the first argument (T_REG) into type char
+// and prints it to standard output.
+//
+// In the original, the output of the aff is switched on using the -a flag.
+
+void	vm_instr_aff(
+		t_arena *a,
+		t_process *p)
+{
+	t_size	mem_i;
+	t_uint8	acb;
+	char	out;
+
+	mem_i = (p->pc + 1) % MEM_SIZE;
+	acb = a->mem[mem_i];
+
+	if (vm_check_acb(acb, 0) != REG_CODE)
+		vm_error("Error arg 1 sti!\n");
+	mem_i = (mem_i + 1) % MEM_SIZE;
+	out = (char)*vm_get_reg_addr(p, mem_i);
+	print("%c", out);
+	mem_i += REG_ADDR_SIZE % MEM_SIZE;
+	p->pc = mem_i;
+}
