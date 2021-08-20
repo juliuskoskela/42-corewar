3122182dbb2a2905238a03716257547b8b538b0a
ksuomala <ksuomala@student.hive.fi>
2021-08-20 14:51:12 +0300

    zjmp instruction

---
+++ vm/src/vm_instr_zjmp.c
@@ -0,0 +1,17 @@
+#include "vm.h"
+
+// aff T_DIR
+// If zf is true, jump T_DIR bytes in memory.
+
+void	vm_instr_zjmp(
+		t_arena *a,
+		t_process *p)
+{
+	t_size	jump;
+
+	if (!p->zf)
+		jump = DIR_VAL_SIZE + 1;
+	else
+		jump = vm_get_val(a, p, DIR_CODE, (p->pc + 1) % MEM_SIZE) % IDX_MOD;
+	p->pc = (p->pc + jump) % MEM_SIZE;
+}
