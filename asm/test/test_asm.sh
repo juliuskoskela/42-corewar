c8e95c35bb6703423d170239063c2a42a87c8bde
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-08-24 17:08:18 +0300

    asm: add error testing files

--- asm/test/test_asm.sh
+++ asm/test/test_asm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-executable="subject_asm"
-test_dir="asm_test_files"
+executable="../asm"
+test_dir="asm_test_error_files"
 
-for file in $test_dir/*.s
+for file in $test_dir/*
 do
-	echo "Test ./$executable $file:"
-	./$executable $file
+	echo "Test $executable $file:"
+	$executable $file
 	echo ""
 done
