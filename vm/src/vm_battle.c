6cbd923aded5ba2898fd70fbd665c8ab222fc4ac
Julius Koskela <julkoske@gmail.com>
2021-08-31 12:24:49 +0300

    Current cycle comparison to dump nbr cycles was wrong direction

--- vm/src/vm_battle.c
+++ vm/src/vm_battle.c
@@ -42,14 +42,14 @@ void	vm_battle(t_arena arena)
 		while (++arena.cycles_since_check < arena.cycle_to_die)
 		{
 			vm_execute_cycle(arena.processes, &arena);
-			if (arena.current_cycle < arena.dump_nbr_cycles)
+			if (arena.current_cycle > arena.dump_nbr_cycles)
 			{
 				vm_print_arena(arena, arena.processes);
 				vm_free_processes(&arena.processes);
 				return ;
 			}
-			if (arena.pause_nbr_cycles && \
-			arena.current_cycle + 1 % arena.pause_nbr_cycles)
+			if (arena.pause_nbr_cycles
+				&& arena.current_cycle + 1 % arena.pause_nbr_cycles)
 				vm_pause_and_print_memory(arena);
 		}
 		vm_check_live(&arena.processes, &arena);
