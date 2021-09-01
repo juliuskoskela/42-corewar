e7876de5d3e498bc48d77db187e80a94daadad5e
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-09-01 08:34:48 +0300

    vm: switch vm_get_reg_addr reg number check to use REG_NUMBER macro

--- vm/src/vm_get_reg_addr.c
+++ vm/src/vm_get_reg_addr.c
@@ -4,9 +4,9 @@ t_reg_addr vm_get_reg_addr(
 		t_process *p,
 		t_size i)
 {
-	if (i < 1 || i > 16)
+	if (i < 1 || i > REG_NUMBER)
 	{
-		print("Reg out of bounds!\n");
+		print("Reg %d out of bounds!\n", (int)i);
 		return (NULL);
 	}
 	return (&p->registers[i - 1]);
