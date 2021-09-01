fad3cb1dc7be27e9a17af804a5152ac631955baa
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-09-01 08:29:49 +0300

    vm: split vm_get_arg_type to its own file, move PC movement verbosity prints t o vm_advance_pc, switch register types from unsigned to signed

--- vm/src/vm_advance_pc.c
+++ vm/src/vm_advance_pc.c
@@ -1,6 +1,11 @@
 #include "vm.h"
 
-void	vm_advance_pc(t_size *pc, int size)
+void	vm_advance_pc(t_size *pc, int size, int verbosity)
 {
+	t_size	new_pc;
+
+	new_pc = (*pc + size) % MEM_SIZE;
+	if ((verbosity & VM_VERBOSE_PC) != 0)
+		print("\t\tPC: %d => %d\n", (int)*pc, (int)new_pc);
 	*pc = (*pc + size) % MEM_SIZE;
 }
