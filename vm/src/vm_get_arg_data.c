7bcc94bf26190e3c685943411cfca293f6b12a72
Julius <julkoske@gmail.com>
2021-09-03 13:52:13 +0300

    Parameters were in wrong order in caller at line 38

--- vm/src/vm_get_arg_data.c
+++ vm/src/vm_get_arg_data.c
@@ -1,27 +1,27 @@
 #include "vm.h"
 
 t_uint8	vm_get_arg_size(
+		t_uint8 acb,
 		t_uint8 opcode,
-		t_uint8 arg_nbr,
-		t_uint8 acb)
+		t_uint8 arg_i)
 {
-	if (opcode == 2 && arg_nbr == 1 && vm_get_arg_type(acb, 1) == DIR_CODE)
+	if (opcode == 2 && arg_i == 1 && vm_get_arg_type(acb, 1) == DIR_CODE)
 		return (REG_SIZE);
-	else if (opcode == 6 && (arg_nbr == 1 || arg_nbr == 2) &&
-	vm_get_arg_type(acb, arg_nbr) == DIR_CODE)
+	else if (opcode == 6 && (arg_i == 1 || arg_i == 2) &&
+	vm_get_arg_type(acb, arg_i) == DIR_CODE)
 		return (REG_SIZE);
-	else if (opcode == 7 && (arg_nbr == 1 || arg_nbr == 2) &&
-	vm_get_arg_type(acb, arg_nbr) == DIR_CODE)
+	else if (opcode == 7 && (arg_i == 1 || arg_i == 2) &&
+	vm_get_arg_type(acb, arg_i) == DIR_CODE)
 		return (REG_SIZE);
-	else if (opcode == 8 && (arg_nbr == 1 || arg_nbr == 2) &&
-	vm_get_arg_type(acb, arg_nbr) == DIR_CODE)
+	else if (opcode == 8 && (arg_i == 1 || arg_i == 2) &&
+	vm_get_arg_type(acb, arg_i) == DIR_CODE)
 		return (REG_SIZE);
-	else if (opcode == 14 && (arg_nbr == 1 || arg_nbr == 2) &&
-	vm_get_arg_type(acb, arg_nbr) == DIR_CODE)
+	else if (opcode == 14 && (arg_i == 1 || arg_i == 2) &&
+	vm_get_arg_type(acb, arg_i) == DIR_CODE)
 		return (REG_SIZE);
-	else if (vm_get_arg_type(acb, arg_nbr) == DIR_CODE)
+	else if (vm_get_arg_type(acb, arg_i) == DIR_CODE)
 		return (DIR_VAL_SIZE);
-	else if (vm_get_arg_type(acb, arg_nbr) == IND_CODE)
+	else if (vm_get_arg_type(acb, arg_i) == IND_CODE)
 		return (IND_ADDR_SIZE);
 	else
 		return (REG_SIZE);
