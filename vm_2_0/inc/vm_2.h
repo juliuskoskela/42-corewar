2c2e5ab4a6ab1b6f95bfefba503c3a7be49280ea
ksuomala <ksuomala@student.hive.fi>
2021-10-02 14:37:56 +0300

    vm_2.0 directory

---
+++ vm_2_0/inc/vm_2.h
@@ -0,0 +1,70 @@
+#ifndef VM_H
+# define VM_H
+
+# include "../../inc/corewar.h"
+
+# define CYCLE_TO_DIE			1536
+
+typedef struct s_reg
+{
+	t_byte		mem[REG_SIZE];
+	t_size		len;
+}	t_reg;
+
+typedef struct s_arg
+{
+	t_reg		data;
+	t_uint8		type;
+	t_uint8		promoted;
+}	t_arg;
+
+typedef struct s_instr
+{
+	t_op		*op;
+	t_arg		opcode;
+	t_arg		acb;
+	t_arg		args[3];
+}	t_instr;
+
+typedef struct s_player
+{
+	t_header	header;
+	t_byte		program[CHAMP_MAX_SIZE];
+}	t_player;
+
+typedef struct s_acb
+{
+	t_uint8		arg[3];
+}	t_acb;
+
+typedef struct s_buff
+{
+	t_byte		*mem;
+	t_size		pos;
+	t_size		len;
+}	t_buff;
+
+
+typedef struct s_process
+{
+	t_uint32	id;
+	t_size		pc;
+	t_bool		zf;
+	t_int32		last_live;
+	t_int32		cycles_before_execution;
+	t_instr		*current_instruction;
+	t_reg		registers[REG_NUMBER];
+}	t_process;
+
+typedef struct s_arena
+{
+	t_player	players[MAX_PLAYERS];
+	t_size		player_count;
+	t_buff		buffer;
+	t_size		offset;
+	t_process	*processes;
+	t_size		current_cycle;
+	t_size		cycle_to_die;
+}	t_arena;
+
+#endif
---
+++ vm_2_0/src/vm_main_2.c
@@ -0,0 +1,24 @@
+#include "vm_2.h"
+
+void	vm_init_arena(t_arena *arena)
+{
+	mzero(arena, sizeof(t_arena));
+	arena->cycle_to_die = CYCLE_TO_DIE;
+	arena->current_cycle = 1;
+}
+
+int	main(int argc, char **argv)
+{
+	t_arena	arena;
+
+	// if (argc < 2)
+	// {
+	// 	print("usage: vm [player_count][.cor]\n");
+	// 	return (0);
+	// }
+	vm_init_arena(&arena);
+	vm_save_input(&arena, argc, argv);
+//	vm_test_print_arena(arena); //test
+	vm_battle(arena);
+	return (0);
+}
