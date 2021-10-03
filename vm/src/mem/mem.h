891426d9b0905ef7af36bf2ba42eeb32918dabd2
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-10-03 17:27:01 +0300

    rename vm_mem_set to vm_mem_set_pos

--- vm/src/mem/mem.h
+++ vm/src/mem/mem.h
@@ -16,7 +16,7 @@ t_mem	*vm_mem_new(t_mem *src, t_size len);
 void	vm_mem_print_overlay(t_mem *src, t_size start, t_size len, char *colour);
 void	vm_mem_print(t_mem *src);
 t_bool	vm_mem_read(t_byte *dst, t_mem *src, size_t dst_len);
-t_mem	*vm_mem_set(t_mem *src, t_size pos);
+t_mem	*vm_mem_set_pos(t_mem *src, t_size pos);
 t_bool	vm_mem_write(t_mem *dst, t_byte *src, size_t src_len);
 t_bool	vm_mem_read(t_byte *dst, t_mem *src, size_t dst_len);
 void	vm_mem_increment_pos(t_mem *src, t_size i);
