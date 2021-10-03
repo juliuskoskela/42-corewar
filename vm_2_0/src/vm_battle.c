1adcdb897f36a68c1d7c599be6284b1fd9998e90
ksuomala <ksuomala@student.hive.fi>
2021-10-03 15:43:39 +0300

    comment old loop

--- vm_2_0/src/vm_battle.c
+++ vm_2_0/src/vm_battle.c
@@ -31,33 +31,33 @@ t_process	*init_processes(t_arena arena)
 ** battle.cycle_to_die cycles.
 */
 
-void	vm_battle(t_arena arena)
-{
-	arena.processes = init_processes(arena);
-//	vm_test_print_processes(arena.processes);
-	vm_introduce_champs(arena);
-	while (arena.processes)
-	{
-		vm_execute_cycle(arena.processes, &arena);
-		while (++arena.cycles_since_check < arena.cycle_to_die)
-		{
-			if (arena.dump_nbr_cycles &&
-			arena.current_cycle > arena.dump_nbr_cycles)
-			{
-				vm_print_arena(arena, arena.processes);
-				vm_free_processes(&arena.processes);
-				return ;
-			}
-			vm_execute_cycle(arena.processes, &arena);
-			if (arena.pause_nbr_cycles
-				&& arena.current_cycle + 1 % arena.pause_nbr_cycles)
-				vm_pause_and_print_memory(arena);
-		}
-		vm_check_live(&arena.processes, &arena);
-	}
-	print("Player %d (%s) won\n", arena.last_player_alive,
-		arena.players[arena.last_player_alive - 1].prog_name);
-}
+// void	vm_battle(t_arena arena)
+// {
+// 	arena.processes = init_processes(arena);
+// //	vm_test_print_processes(arena.processes);
+// 	vm_introduce_champs(arena);
+// 	while (arena.processes)
+// 	{
+// 		vm_execute_cycle(arena.processes, &arena);
+// 		while (++arena.cycles_since_check < arena.cycle_to_die)
+// 		{
+// 			if (arena.dump_nbr_cycles &&
+// 			arena.current_cycle > arena.dump_nbr_cycles)
+// 			{
+// 				vm_print_arena(arena, arena.processes);
+// 				vm_free_processes(&arena.processes);
+// 				return ;
+// 			}
+// 			vm_execute_cycle(arena.processes, &arena);
+// 			if (arena.pause_nbr_cycles
+// 				&& arena.current_cycle + 1 % arena.pause_nbr_cycles)
+// 				vm_pause_and_print_memory(arena);
+// 		}
+// 		vm_check_live(&arena.processes, &arena);
+// 	}
+// 	print("Player %d (%s) won\n", arena.last_player_alive,
+// 		arena.players[arena.last_player_alive - 1].prog_name);
+// }
 
 void vm_battle(t_arena arena)
 {
