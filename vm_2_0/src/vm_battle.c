f98a76c41649ec17f4505e9a6bb8bf020f98516d
ksuomala <ksuomala@student.hive.fi>
2021-10-03 15:43:01 +0300

    make battle loop more compact

--- vm_2_0/src/vm_battle.c
+++ vm_2_0/src/vm_battle.c
@@ -44,7 +44,7 @@ void	vm_battle(t_arena arena)
 			if (arena.dump_nbr_cycles &&
 			arena.current_cycle > arena.dump_nbr_cycles)
 			{
-//				vm_print_arena(arena, arena.processes);
+				vm_print_arena(arena, arena.processes);
 				vm_free_processes(&arena.processes);
 				return ;
 			}
@@ -54,6 +54,30 @@ void	vm_battle(t_arena arena)
 				vm_pause_and_print_memory(arena);
 		}
 		vm_check_live(&arena.processes, &arena);
+	}
+	print("Player %d (%s) won\n", arena.last_player_alive,
+		arena.players[arena.last_player_alive - 1].prog_name);
+}
+
+void vm_battle(t_arena arena)
+{
+	arena.processes = init_processes(arena);
+	vm_introduce_champs(arena);
+	while (arena.processes)
+	{
+		// make sure to increment arean.cycles_since_check!!
+		vm_execute_cycle(arena.processes, &arena);
+		if (++arena.cycles_since_check >= arena.cycle_to_die)
+			vm_check_live(&arena.processes, &arena);
+		if (arena.current_cycle > arena.dump_nbr_cycles)
+		{
+			vm_print_arena(arena, arena.processes);
+			vm_free_processes(&arena.processes);
+			return ;
+		}
+		if (arena.pause_nbr_cycles
+			&& arena.current_cycle + 1 % arena.pause_nbr_cycles)
+			vm_pause_and_print_memory(arena);
 	}
 	print("Player %d (%s) won\n", arena.last_player_alive,
 		arena.players[arena.last_player_alive - 1].prog_name);
