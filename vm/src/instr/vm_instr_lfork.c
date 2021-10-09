db24daa0aeebfca1b3d3db7eb61b520a8a387085
ksuomala <ksuomala@Kalles-MacBook-Pro.local>
2021-10-09 18:25:16 +0300

    add lfork instr

--- vm/src/instr/vm_instr_lfork.c
+++ vm/src/instr/vm_instr_lfork.c
@@ -9,7 +9,7 @@ void	vm_instr_fork(t_arena *a, t_process *p)
 	offset = vm_reg_deref(&offset, p->current_instr.args[0]);
 	new = vm_create_process(a, p, p->id);
 	//modify pc accordingly.
-	new.pc = (p->pc + (offset % IDX_MOD)) % MEM_SIZE;
+	new.pc = (p->pc + offset) % MEM_SIZE;
 	//add new process to the end of the list
 	while (p->next)
 		p = p->next;
