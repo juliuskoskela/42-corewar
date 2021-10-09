69a0eedd3411ea69d1d8897aaf3e75b95a9095ab
ksuomala <ksuomala@Kalles-MacBook-Pro.local>
2021-10-09 20:22:51 +0300

    add process register initialisation

--- vm/src/vm_create_process.c
+++ vm/src/vm_create_process.c
@@ -8,11 +8,19 @@ t_process	*vm_create_process(t_arena arena, t_process *process_lst, \
 t_int32 player_id)
 {
 	t_process	*new_process;
+	int			i;
 
+	i = 0;
 	new_process = minit(sizeof(t_process));
 	if (!new_process)
 		vm_exit_error("Malloc failed in create_process\n");
 	new_process->id = player_id;
+	while (i < REG_NUMBER)
+	{
+		vm_reg_set(&new_process->registers[i], REG_SIZE);
+		i++;
+	}
+	vm_reg_ref((t_reg *)&new_process->registers[i], (t_byte *)&player_id);
 	vm_reverse_bytes(&new_process->registers[0], &player_id, REG_SIZE);
 	new_process->pc = (player_id - 1) * arena.offset;
 	new_process->cycles_before_execution = -1;
