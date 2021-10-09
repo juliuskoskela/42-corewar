671ee252eeebca7afe4c1d32a222277a14c06848
ksuomala <ksuomala@Kalles-MacBook-Pro.local>
2021-10-09 18:15:14 +0300

    add fork instr

---
+++ vm/src/instr/vm_instr_fork.c
@@ -0,0 +1,18 @@
+#include "vm.h"
+
+void    vm_instr_fork(t_arena *a, t_process *p)
+{
+    t_int32     offset;
+    t_process   *new;
+
+    //Save first argument T_DIR value
+    offset = vm_reg_deref(&offset, p->current_instr.args[0]);
+
+    new = vm_create_process(a, p, p->id);
+    //modify pc accordingly.
+    new.pc = p->pc + (offset % IDX_MOD);
+    //add new process to the end of the list
+    while (p->next)
+        p = p->next;
+    p->next = new;
+}
