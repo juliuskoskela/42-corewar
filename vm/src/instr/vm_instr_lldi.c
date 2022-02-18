c9948f085424da7422b17501d541e588cfc85498
ksuomala <ksuomala@Kalles-MacBook-Pro.local>
2022-02-18 16:31:39 +0000

    add first scetch of lldi. Mostly copied from ldi

--- vm/src/instr/vm_instr_lldi.c
+++ vm/src/instr/vm_instr_lldi.c
@@ -12,12 +12,30 @@
 
 void	vm_instr_lldi(t_arena *a, t_process *p)
 {
+	t_int32 lhs;
+	t_int32 rhs;
+	t_int32 mem_addr;
+	t_int32 reg_addr;
+
+	// Get register
+	vm_reg_store((t_byte *)&reg_addr, &p->current_instruction.args[2].data);\
+
+	if (reg_addr > 16)
+	{
+		print("reg addr > 16\n");
+		return ;
+	}
+
 	// Get lhs, rhs
+	vm_instr_get_param(&lhs, a, p, 0);
+	vm_instr_get_param(&rhs, a, p, 1);
 
-	// Calculate index (lhs + rhs) % IDX_MOD
+	// Calculate index lhs + rhs
+	mem_addr = lhs + rhs;
 
-	// Update zf
-
-	// Store value at `index` to register dst
-	vm_instr_null(a, p);
+	// Load value from `mem_addr` to register dst.
+	vm_mem_set_pos(&a->mem, p->pc + mem_addr);
+	vm_mem_read((t_byte *)&p->registers[reg_addr - 1], &a->mem, 4);
+	
+	//update zf
 }
