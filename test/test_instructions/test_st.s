43c745d4e1da40dc002ce7ec33a169890d5c41c6
Satu Koskinen <satu.a.koskinen@gmail.com>
2022-02-19 15:25:30 +0200

    Update tests & gitignore

--- test/test_instructions/test_st.s
+++ test/test_instructions/test_st.s
@@ -5,15 +5,27 @@
 
 # Stores the value of registry src in dst
 
-# x cycles
+# 5 cycles
 
 # { T_REG, T_IND | T_REG }
 
 l2:		st r1, :live
 
 st r1, 100
-st r16, 2
-st r10, r1
+st r16, 200
+
+st r1, 0
+
+st r10, r12
 st r2, r2
+st r1, r16
+st r1, r15
+
+st r1, :l2
+
+st r1, -100
+st r10, -1
 
 live:	live %1
+
+
