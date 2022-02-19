580b972696d2c1c7cc7b2ded5880c1666edc813f
Satu Koskinen <satu.a.koskinen@gmail.com>
2022-02-19 15:25:59 +0200

    Add vm_print_process

---
+++ vm/src/vm_print_process.c
@@ -0,0 +1,20 @@
+#include "vm.h"
+
+void    vm_print_process(t_process *p)
+{
+    t_size i;
+
+    print("id: %d pc: %d zf: %d\n", (int)p->id, (int)p->pc, (int)p->zf);
+    print("last live: %d\n", (int)p->last_live);
+    print("cycles before exec: %d\n", (int)p->cycles_before_execution);
+    if (p->current_instruction.op != NULL)
+        print("current instruction: %s\n", p->current_instruction.op->mnemonic);
+    i = 0;
+    while (i < 16)
+    {
+        print("R%d\t", i + 1);
+        vm_reg_print(&p->registers[i]);
+        print("\n");
+        i++;
+    }
+}
