d2f89ce55b052aa244d2c4038b6e31f245965e87
ksuomala <ksuomala@Kalles-MacBook-Pro.local>
2022-02-19 14:47:40 +0000

    update live test file

--- test/test_instructions/test_live.s
+++ test/test_instructions/test_live.s
@@ -11,11 +11,14 @@
 
 
 live %742
-live %1
+live %-1
 live %3
-live %0
-live %100
 live %-15
 live %100001029
 live %0
+live %2
+live %-2
+live %3
+
+# only players 1 and 2 should be stated alive.
 
