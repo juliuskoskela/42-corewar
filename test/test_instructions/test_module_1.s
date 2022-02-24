29f0363f83fad102042ce61a37bb0659ac2b5654
ksuomala <ksuomala@student.hive.fi>
2022-02-24 13:42:01 +0000

    add live to test files

--- test/test_instructions/test_module_1.s
+++ test/test_instructions/test_module_1.s
@@ -1,13 +1,10 @@
-# ld T_DIR src, T_IND, T_REG dst
-# Loads src in register dst, value of src affects zf
-# 5 cycles
-
-.name "TEST_LD"
-.comment "Tests ld"
+.name "TEST_MODULE_ONE"
+.comment "Tests instructions with value 516"
 
 ld %516, r2
 st r2, 516
 zjmp %516
 ldi %258, %258, r2
 sti r2, %258, %258
-fork %516 
+fork %516
+live %-1
