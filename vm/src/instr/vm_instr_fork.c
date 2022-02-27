bd68417be80080cba9048c4a6f8081391c4189e7
ksuomala <ksuomala@student.hive.fi>
2022-02-27 13:14:22 +0000

    each process now has a unique id

--- vm/src/instr/vm_instr_fork.c
+++ vm/src/instr/vm_instr_fork.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vm_instr_fork.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: satukoskinen <satukoskinen@student.42.f    +#+  +:+       +#+        */
+/*   By: ksuomala <ksuomala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 20:14:26 by satukoskine       #+#    #+#             */
-/*   Updated: 2022/02/25 20:14:27 by satukoskine      ###   ########.fr       */
+/*   Updated: 2022/02/27 13:09:57 by ksuomala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ void	vm_instr_fork(t_arena *a, t_process *p)
 	mcpy(new, p, sizeof(t_process));
 	new->pc = (t_size)(((int)p->pc + (offset % IDX_MOD)) % MEM_SIZE);
 	new->next = a->processes;
+	new->id = a->processes->id + 1;
 	new->cycles_before_execution = -1;
 	mzero(&new->current_instruction, sizeof(t_instr));
 	a->processes = new;
